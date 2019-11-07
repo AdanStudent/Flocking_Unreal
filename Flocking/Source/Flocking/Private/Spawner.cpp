b0ff2574ff1ae2a7a39c954a351e635adfe50417
Adan <programmerAdan@gmail.com>
2019-11-07 15:19:28 -0600

    Update Spawner.cpp

--- Flocking/Source/Flocking/Private/Spawner.cpp
+++ Flocking/Source/Flocking/Private/Spawner.cpp
@@ -11,6 +11,7 @@ ASpawner::ASpawner()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	Tree = CreateDefaultSubobject<UOctTree>(TEXT("Tree"));
 
 	auto t = Tree->Boundary;
 }
